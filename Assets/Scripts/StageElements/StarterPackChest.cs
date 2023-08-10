c7ed8438b04c23c78e550993c76a76620b4b8eb1
Anthony Hoang <anthonyhoang3074@gmail.com>
2023-08-10 16:07:48 -0500

    Made number of recipes given by starter pack adjustable

--- Assets/Scripts/StageElements/StarterPackChest.cs
+++ Assets/Scripts/StageElements/StarterPackChest.cs
@@ -11,11 +11,16 @@ public class StarterPackChest : ItemChest
     [SerializeField]
     [Range(1, 5)]
     private int numIngredientDrops = 2;
+    [SerializeField]
+    [Min(1)]
+    private int numRecipeDrops = 1;
 
 
     // On awake, set up item chest
     private void Awake() {
-        addItem(recipeBookPage);
+        for (int r = 0; r < numRecipeDrops; r++) {
+            addItem(recipeBookPage);
+        }
         
         for(int i = 0; i < numIngredientDrops; i++) {
             addItem(ingredientDrops.getLootDrop());
